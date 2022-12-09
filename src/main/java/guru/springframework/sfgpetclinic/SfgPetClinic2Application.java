91eb946ed114a2f87039882f9edc826771196606
mthbs <maxim.neumann@gmx.net>
2022-12-09 14:13:10 +0100

    Initial Project Add

---
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinic2Application.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SfgPetClinic2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinic2Application.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sfgpetclinic/SfgPetClinic2ApplicationTests.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SfgPetClinic2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
