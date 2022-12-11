15c7e28b955b2a3c60e7ece0c1ea731f4f2fb5e2
mthbs <maxim.neumann@gmx.net>
2022-12-11 16:38:44 +0100

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+}
